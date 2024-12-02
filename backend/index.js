c3f3bbd8611e72dcb98b5adbc8c76cf4b40d8d09
Wix-Code <ogbonna428alex@gmail.com>
2024-12-02 13:28:46 +0100

    push

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000', // allow requests from this origin
+  origin: ['https://realtor-frontend-xi.vercel.app'], // allow requests from this origin
   credentials: true, // allow cookies
 }));
 
